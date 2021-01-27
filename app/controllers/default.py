2fd9513adab126b994177a9a1dc11d9978ccbe95
Mariana Narita Masunaga Nara Mendes Benitez <marynarita@gmail.com>
2021-01-27 10:33:19 -0400

    Validação de login

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,15 +1,24 @@
-from app import app
 from flask import render_template
+from app import app
 
+from app.models.forms import LoginForm
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def logar():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.email.data)
+        print(form.senha.data)
+    else:
+        print(form.errors)
+
+    return render_template("login.html",
+                            form=form)
 
 
 @app.route("/cadastro")
